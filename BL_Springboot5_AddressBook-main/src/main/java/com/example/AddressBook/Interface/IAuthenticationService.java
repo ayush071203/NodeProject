d5d903656f5ae74d24ce5682bf928513b7fe1158
Ayush <133644378+ayush071203@users.noreply.github.com>
2025-03-18 23:59:46 +0530

    Delete BL_Springboot5_AddressBook-main directory

--- BL_Springboot5_AddressBook-main/src/main/java/com/example/AddressBook/Interface/IAuthenticationService.java
+++
@@ -1,9 +0,0 @@
-package com.example.AddressBook.Interface;
-
-
-import com.example.AddressBook.dto.AuthUserDTO;
-import com.example.AddressBook.model.AuthUser;
-public interface IAuthenticationService {
-    AuthUser register(AuthUserDTO userDTO) throws Exception;
-
-}
--- BL_Springboot5_AddressBook-main/src/main/java/com/example/AddressBook/exception/ContactNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.AddressBook.exception;
-
-public class ContactNotFoundException extends RuntimeException {
-    public ContactNotFoundException(String message) {
-        super(message);
-    }
-}
